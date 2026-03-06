03d3b11ff5d8a4e04b585b20f521d8bca8e5afa1
SamCorp <Manhcuong5311@gmail.com>
2026-03-06 12:31:40 +0700

    Initial Commit

---
+++ Structify/StructifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  StructifyApp.swift
+//  Structify
+//
+//  Created by Sam Manh Cuong on 6/3/26.
+//
+
+import SwiftUI
+
+@main
+struct StructifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
